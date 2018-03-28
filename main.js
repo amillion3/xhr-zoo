4e54e9ca511caf7009f163b7c6fcd928cd58ce2a
Andy <amillion3@gmail.com>
2018-03-27 19:41:44 -0500

    finished issue5, added escape buttons and background color changes

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-/*Write startApplication function - sets up xhr request and executes
-Write executeThisCodeIfXHRFails function that consoles that an error has occured
-Write executeThisCodeAfterFileLoaded function that parses xhr response and passes it to a buildDomString function
-Write buildDomString function that loops over input array and creates h1 tags with name and adds them to a big string then calls printToDom and passes string
-write printToDom function that takes a string and an id and writes the string to the id*/
-
 const printToDom = (domString, divId) => {
   document.getElementById(divId).innerHTML = domString;
 };
@@ -21,7 +15,7 @@ const buildDomString = (inputArray) => {
                 <h3>${inputArray[i].number}</h3>
                 <img class = "animal-image"src="${inputArray[i].imageURL}">
                 <div class="button-container">
-                  <button class="btn-escape">Escaped!</button>
+                  <button class="escaped">Escaped!</button>
                 </div>
               </div>
     `;
@@ -29,12 +23,45 @@ const buildDomString = (inputArray) => {
   printToDom(output, "zoo-wrapper");
 };
 
+const addEscapedEventListeners = () => {
+  const escapedButtons = document.getElementsByClassName("escaped");
+  for (let i = 0; i < escapedButtons.length; i++) {
+    escapedButtons[i].addEventListener('click', animalEscaped);
+  }
+  //animalEscaped();
+};
+
+const animalEscaped = () => {
+
+  showCarnivores();
+  showVegetables();
+};
+
+const showCarnivores = () => {
+  const carnivores = document.getElementsByClassName("carnivore");
+  for (let i = 0; i < carnivores.length; i++) {
+    carnivores[i].children[3].innerHTML = "";
+    carnivores[i].classList.add('red'); //turns carnivores' background color red
+  }
+};
+
+const showVegetables = () => {
+  const vegetable = document.getElementsByClassName("vegetable");
+  for (let i = 0; i < vegetable.length; i++) {
+    vegetable[i].children[3].innerHTML = `<button class="eatMe">EAT ME!</button>`;
+    vegetable[i].classList.add('green'); //turns vegetables' background color green
+  }
+};
+
+
+// XHR Below
 //CANNOT BE ES6 OR A FAT ARROW FUNCTION. ES6 allowed inside function.
 function executeThisCodeAfterFileLoaded() {
   console.log("line 20");
   const parsedData = JSON.parse(this.responseText);
   console.log(parsedData);
   buildDomString(parsedData.animals); //Grabs array from the object
+  addEscapedEventListeners(); //Cards are built, now you can add eventListeners
 };
 
 const executeThisCodeIfXHRFails = () => {
